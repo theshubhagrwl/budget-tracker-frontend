85928adbb22c0b4f29b5a4d2422089f45f0da9b0
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-09-06 11:12:44 +0530

    improved home page

--- src/core/WelcomePage.js
+++ src/core/WelcomePage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Base from "./Base";
 import { Typography, Box, Button, ButtonGroup } from "@material-ui/core";
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { Link } from "react-router-dom";
-
-import "../styles.css";
+// import "../styles.css";
 import { makeStyles } from "@material-ui/core/styles";
+import { isAuthenticated } from "../auth";
 
 const useStyles = makeStyles((theme) => ({
   detail: {
@@ -13,11 +12,8 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1.5rem",
     margin: "1rem",
   },
-  actionButton: {
-    backgroundColor: "#212121",
-    margin: "1rem",
-    color: "white",
-  },
+  actionButton: theme.blackButton,
+  greenButton: theme.greenButton,
   mainDiv: theme.flexDiv,
 }));
 
@@ -31,24 +27,45 @@ const WelcomePage = () => {
         </Typography>
         <Box component="div">
           <Typography variant="h5" component="h5" className={classes.detail}>
-            Hi, Welcome to the Budget Tracker App <span>😃</span> <br /> Glad to
-            see you here. <br />
-            This app will help you keep track of you budget. <br />
-            <Typography variant="h6" component="h6" style={{ margin: "1rem" }}>
-              LETS GET STARTED
-            </Typography>
-            {/* <Box component="div" className={classes.buttonGroup}> */}
-            <ButtonGroup orientation="vertical" variant="text">
-              <Link to="/signin" style={{ textDecoration: "none" }}>
-                <Button color="secondary" className={classes.actionButton}>
-                  Login
-                </Button>
-              </Link>
-              <Link to="/signup" style={{ textDecoration: "none" }}>
-                <Button className={classes.actionButton}>Register</Button>
-              </Link>
-            </ButtonGroup>
+            Hi, Welcome to the Budget Tracker App{" "}
+            <span role="img" aria-label="smilie">
+              😃
+            </span>
+            <br /> Glad to see you here. <br />
+            This app will help you keep track of your daily budget. <br />
           </Typography>
+          <Typography variant="h6" component="h6" style={{ margin: "1rem" }}>
+            LETS GET STARTED
+          </Typography>
+          <ButtonGroup orientation="vertical" variant="text">
+            <Link to="/signin" style={{ textDecoration: "none" }}>
+              <Button
+                className={classes.greenButton}
+                style={{ marginBottom: "1rem" }}
+              >
+                Login
+              </Button>
+            </Link>
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <Button className={classes.actionButton}>Register</Button>
+            </Link>
+          </ButtonGroup>
+          {isAuthenticated() ? (
+            <Box component="div">
+              <Typography
+                variant="h6"
+                component="h6"
+                style={{ margin: "1rem" }}
+              >
+                GO TO DASHBOARD
+              </Typography>
+              <Link to="/user/dashboard" style={{ textDecoration: "none" }}>
+                <Button className={classes.greenButton}>DASHBOARD</Button>
+              </Link>
+            </Box>
+          ) : (
+            ""
+          )}
         </Box>
       </Box>
     </Base>
